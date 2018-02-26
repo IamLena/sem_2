189cf74c201aaa8ca76b738f96b2f2f9d9cc2864
IamLena <34005469+IamLena@users.noreply.github.com>
2018-02-26 15:07:39 +0300

    updated in class

--- lab_1.py
+++ lab_1.py
@@ -5,7 +5,7 @@
 from tkinter import *
 from math import sqrt
 
-# Окно с именем 'calculator'
+# Окно с именем 'calculator' размера 400*200
 root = Tk()
 root.title('calculator')
 root.minsize(400,200)
@@ -38,19 +38,28 @@ def find_root(a,b,c):
                 m['text'] = 'нет корней'
         else:
             x = -c/b
+            if x == -0.0:
+                x = 0.0
             m['text'] = 'уравнение линейно, x = '+ str(round(x,2))
 
     else:
         D = b*b - 4*a*c
         if D<0:
-            m['text'] = 'Дискриминант меньше нуля, нет корней'
+            m['text'] = 'Дискриминант меньше нуля,\n нет действительных корней'
         elif D==0:
             x = b/(2*a)
+            if x == -0.0:
+                x = 0.0
             m['text'] = 'совпавшие корни, х = ' + str(round(x,2))
         else:
             x1 = (-b+sqrt(D))/(2*a)
+            if x1 == -0.0:
+                x1 = 0.0
             x2 = (-b-sqrt(D))/(2*a)
-            m['text'] = 'x1 = ' + str(round(x1,2)) + '  x2 = ' + str (round(x2, 2))
+            if x2 == -0.0:
+                x2 = 0.0
+            m['text'] = 'x1 = ' + str(round(x1,2)) + '  x2 = ' + str (round\
+                                                                    (x2, 2))
 
 # Прием аргументов у пользователя и их передача в функцию решения    
 def solve ():
@@ -65,28 +74,26 @@ def solve ():
     else:
         m['text'] = 'некорректный ввод'
 
-# Очистить поле ввода
-def clear(x):
-    x.delete(0, END)
-
+# Очистить поля ввода
 def clear_a():
-    clear(a)
+    a.delete(0, END)
 
 def clear_b():
-    clear(b)
+    b.delete(0, END)
 
 def clear_c():
-    clear(c)
+    c.delete(0, END)
 
 def clear_all():
-    clear(a)
-    clear(b)
-    clear(c)
+    a.delete(0, END)
+    b.delete(0, END)
+    c.delete(0, END)
     m["text"] = ""
     
 # виджиты
 f = ("Times New Roman",12)
-lab4 = Label(root, text = "Решение квадратного уравнения", font=("Times New Roman", 16, "bold"))
+lab4 = Label(root, text = "Решение квадратного уравнения",font=("Times New \
+Roman", 16, "bold"))
 a = Entry(root, text = "a", width = 5, font = f)
 lab1 = Label(root, text = "x^2 + ", font = f)
 b = Entry(root, text = "b", width = 5, font = f)
@@ -95,7 +102,10 @@ c = Entry(root, text = "c", width = 5, font = f)
 lab3 = Label(root, text = " = 0", font = f)
 lab5 = Label(root, text = 'ответ: ', font = f)
 m = Label (root, text = '', font = f)
-solveb = Button(root, bg = "black", fg = "white", text = "решить", width = 25, command = solve, font = f)
+solveb = Button(root, bg = "black", fg = "white", text = "решить", width = \
+15, command = solve, font = f)
+clearb = Button(root, bg = "black", fg = "white", text = "очистить", width = \
+15, command = clear_all, font = f)
 
 # размещение в окне
 lab4.place(x = 30, y = 5)
@@ -105,11 +115,12 @@ b.place(x = 160,y = 50)
 lab2.place(x = 210,y = 50)
 c.place(x = 250,y = 50)
 lab3.place(x = 300,y = 50)
-lab5.place(x = 30, y = 105)
-m.place(x = 75, y = 105)
-solveb.place(x = 80, y =150)
+lab5.place(x = 30, y = 95)
+m.place(x = 75, y = 95)
+solveb.place(x = 55, y =150)
+clearb.place(x = 205, y = 150)
 
-# пункт меню о авторе
+# пункт меню об авторе
 def info ():
     root2 = Tk()
     root2.title('info')
@@ -117,7 +128,7 @@ def info ():
 Иу7-21Б \n\nЗадание:\nСоздать калькулятор \
 c графическим интерфейсом,\nрешающий квадратное \
 уравнение. Также в\nпрограмме должно быть меню,\nвыполняющее \
-очистку полей по одному и всех\nсразу, повторяющее\
+очистку полей по одному и всех\nсразу, повторяющее \
 математические\nдействия, выводящее информацию\nо программе и авторе.")
     lab4.pack()
     root2.mainloop()
@@ -137,6 +148,3 @@ clearmenu.add_command(label = "c",command = clear_c)
 clearmenu.add_command(label = "all", command = clear_all)
 
 root.mainloop()
-
-
-
