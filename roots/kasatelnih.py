d6bf32c45eb93ec2125e5d7d0ffde4e82788f900
Lena <helly.luchina@gmail.com>
2018-05-21 00:03:13 +0300

    roots

--- roots/kasatelnih.py
+++ roots/kasatelnih.py
@@ -14,22 +14,20 @@ print("-----------------------------")
 
 def f(x):
     return x * x - 3
+
+def f1(x):
+    return 2 * x
+
 def find_root(a, b, eps):
     if f(a) == 0:
         x = a
     elif f(b) == 0:
         x = b
     else:
-        lb = a
-        rb = b
-        x = x = lb - (rb - lb)*f(lb)/(f(rb) - f(lb))
-
-        while (abs(lb - x) > eps and abs(rb - x) > eps):
-            x = lb - (rb - lb)*f(lb)/(f(rb) - f(lb))
-            if f(x) * f(lb) <= 0:
-                rb = x
-            else:
-                lb = x
+        x0 = a
+        while abs(x - x0) < eps:
+            x = x0 - (f(x0) / f1(x0))
+            x0 = x
     return x
 
 def solve(a, b, eps, step):
@@ -53,3 +51,30 @@ plt.ylabel('y')
 plt.title('f(x)')
 plt.grid()
 plt.show()
+
+def newton_method(a , b, step, e, N):
+    lb = a
+    while lb < b:
+        if b - lb <= step:
+            rb = b   
+        else:
+            rb = lb + step
+        n = 0
+        x0 = lb
+        if f(lb) * f(rb) < 0:
+            while (n < N):
+                n += 1
+                if f1(x0) == 0:
+                    break
+                else:
+                    x1 = x0 - (f(x0) / f1(x0))
+                    if not (lb < x1 < rb): 
+                        break
+                if abs(x1 - x0) < e:
+                    break
+                x0 = x1
+            else:
+                break
+        if f(x0) == 0:
+            n += 1
+        lb += step
